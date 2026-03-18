34305b2865cf89f454d1bd22a7e9f6c8fcb8230d
Gavin <gavinredding19@gmail.com>
2026-03-18 13:19:34 -0400

    NLP orientation assignment

---
+++ python-nlp/nlp-orientation.py
@@ -0,0 +1,13 @@
+import nltk
+from nltk.tokenize import word_tokenize
+
+# open your text file
+with open("yourfile.txt", "r", encoding="utf-8") as file:
+    text = file.read()
+
+# tokenize (split into words)
+words = word_tokenize(text)
+
+# print results
+print("Total words:", len(words))
+print("First 20 words:", words[:20])
